02873f42d38318991a5fca33047943e536291854
Muhammad Abdullah <abdullahtariq52@gmail.com>
2019-04-26 10:23:08 +0500

    update example

--- example/src/App.js
+++ example/src/App.js
@@ -1,11 +1,34 @@
-import React from 'react'
-import { useMyHook } from 'use-infinite-scroll'
+import React, { useState, useEffect } from 'react'
+import useInfiniteScroll from 'use-infinite-scroll'
+import superagent from "superagent";
 
 const App = () => {
-  const example = useMyHook()
+  const [ users, setUsers ] = useState([])
+  const [isFetching, setIsFetching] = useInfiniteScroll(loadUsers)
+
+  useEffect(loadUsers, [])
+
+  function loadUsers() {
+    superagent
+      .get('https://randomuser.me/api/?results=50')
+      .then((results) => {
+        setUsers([...users, ...results.body.results])
+        setIsFetching(false)
+      })
+      .catch((err) => {
+        setIsFetching(false)
+      })
+  }
+  console.log(users)
   return (
     <div>
-      {example}
+      <div style={{backgroundColor: 'white',position: 'fixed', top: '0px', width: '100%'}}> Total Count: {users.length}</div>
+      <ul>
+        {users.map(({email}) => (
+        <li key={email}>{email}</li>
+      ))}
+      </ul>
+      {isFetching ? 'Fetching...' : 'Not Fetching'}
     </div>
   )
 }
